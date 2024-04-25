60ed7309c2e809ae792b736c5a52f6abd3b59236
Thibaud-LAMON <thib.lamon@gmail.com>
2024-04-25 14:22:52 +0200

    creating jwt service

--- src/main/java/tlamon/perso/secondaVita/config/JwtAuthFilter.java
+++ src/main/java/tlamon/perso/secondaVita/config/JwtAuthFilter.java
@@ -15,12 +15,26 @@ import java.io.IOException;
 @RequiredArgsConstructor // permet de générer un constructeur avec les propriétés final
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService; // injecte le service JwtService
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request, // récupère la requête
             @NonNull HttpServletResponse response, // récupère la réponse
             @NonNull FilterChain filterChain // permet de passer la requête à la suite du traitement
     ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization"); // récupère le header Authorization
+        final String jwt;
+        final String userName;
 
+        // si le header Authorization est null ou ne commence pas par "Bearer "
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            // on passe à la suite du traitement
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7); // on récupère le token JWT sans le "Bearer "
+        userName = jwtService.extractUserName(jwt);
     }
 }
---
+++ src/main/java/tlamon/perso/secondaVita/config/JwtService.java
@@ -0,0 +1,11 @@
+package tlamon.perso.secondaVita.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String token) {
+        return null;
+    }
+}
