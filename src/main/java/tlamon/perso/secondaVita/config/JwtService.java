135536cb53ea2bc12e9caa17b170bef1aafb3b62
Thibaud-LAMON <thib.lamon@gmail.com>
2024-04-26 12:10:53 +0200

    adding jsonwebtoken dependencies and extract claims methods

--- src/main/java/tlamon/perso/secondaVita/config/JwtService.java
+++ src/main/java/tlamon/perso/secondaVita/config/JwtService.java
@@ -1,11 +1,41 @@
 package tlamon.perso.secondaVita.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "FFD6A45674D659D1498F8FA92F8DC8238DF5B26C1300F3FA4F956EDC1CC2BDEE";
+
     public String extractUserName(String token) {
         return null;
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
