a9dc3f0fcd3350c97b03dde2c7f82c9c1398dcff
Thibaud-LAMON <thib.lamon@gmail.com>
2024-05-26 18:29:38 +0200

    token methods

--- src/main/java/tlamon/perso/secondaVita/config/JwtService.java
+++ src/main/java/tlamon/perso/secondaVita/config/JwtService.java
@@ -2,25 +2,57 @@ package tlamon.perso.secondaVita.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    //clé générée sur https://asecuritysite.com/encryption/plain
     private static final String SECRET_KEY = "FFD6A45674D659D1498F8FA92F8DC8238DF5B26C1300F3FA4F956EDC1CC2BDEE";
 
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject); // récupère le sujet du token
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())// Username peut être un mail, un pseudo, etc
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // le token expire dans 24h
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) //dépréciées : devront être changées
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
