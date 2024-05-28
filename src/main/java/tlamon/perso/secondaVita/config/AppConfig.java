e39de398d9fcb4362861a5f87b3ea66bb0f39adb
Thibaud-LAMON <thib.lamon@gmail.com>
2024-05-28 23:42:00 +0200

    security filter chain

--- src/main/java/tlamon/perso/secondaVita/config/AppConfig.java
+++ src/main/java/tlamon/perso/secondaVita/config/AppConfig.java
@@ -3,19 +3,35 @@ package tlamon.perso.secondaVita.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import tlamon.perso.secondaVita.repositories.UserRepository;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
-    // reprendre à 1:21:23
 
     private final UserRepository userRepository;
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByMail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProv = new DaoAuthenticationProvider();
+        authProv.setUserDetailsService(userDetailsService());
+        authProv.setPasswordEncoder(passEncoder());
+        return authProv;
+    }
+
+    @Bean
+    public PasswordEncoder passEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/tlamon/perso/secondaVita/config/SecuConfig.java
@@ -0,0 +1,61 @@
+package tlamon.perso.secondaVita.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static tlamon.perso.secondaVita.models.user.Roles.ADMIN;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecuConfig {
+
+    private static final String[] WHITE_LIST = {
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/api/v1/auth/logout",
+            "/api/v1/auth/refresh"
+    };
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    //private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers("/api/v1/users/**").hasAnyRole(ADMIN.name())
+                                //.requestMatchers(GET, "/api/v1/users/admin/**").hasAnyAuthority("ROLE_" + ADMIN_READ.name())
+                                //.requestMatchers(POST, "/api/v1/users/admin/**").hasAnyAuthority("ROLE_" + ADMIN_CREATE.name())
+                                //.requestMatchers(PUT, "/api/v1/users/admin/**").hasAnyAuthority("ROLE_" + ADMIN_UPDATE.name())
+                                //.requestMatchers(DELETE, "/api/v1/users/admin/**").hasAnyAuthority("ROLE_" + ADMIN_DELETE.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout
+                        -> logout.logoutSuccessUrl("/api/v1/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication)
+                                -> SecurityContextHolder.clearContext()));
+
+        return http.build();
+    }
+}
